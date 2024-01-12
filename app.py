69fd1e404997bb8adab78fb4157fdb9f9d4f90bf
rishi7032 <49578828+rishi7032@users.noreply.github.com>
2024-01-12 18:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,5 @@ def generate(camera):
 def video_feed():
     return Response(generate(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route("/video")
-def video():
-    return render_template("video.html")
-
 if __name__ == '__main__':
     app.run(debug=True)
